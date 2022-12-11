d0070c7eea15239f6fabd90205e770a30681e66f
Yang Wang <yangwang1@microsoft.com>
2022-12-11 11:07:50 +0800

    update script

--- deploy_superbench.sh
+++ deploy_superbench.sh
@@ -41,37 +41,48 @@ run_command(){
 # @RetVal       : null
 ####
 deploy_superbench(){
-    sudo apt-get update -y       \
-        && sudo apt-get install -y   \
-        --no-install-recommends      \
-        curl                         \
-        wget                         \
-        python3.8                    \
-        python3-pip                  \
-        python3.8-venv               \
-        make                         \
-        ansible
+    # install dependencies
+    sudo apt-get update -y \
+        && sudo apt-get -y install \
+        python3.7-venv
 
-    # cd /usr/bin/                 \
-    # && sudo rm python3           \
-    # && sudo ln -s /usr/bin/python3.8 python3
+    # link python3.7 as python3
+    sudo rm -rf /usr/bin/python3 \
+        && sudo  ln -s /usr/bin/python3.7 /usr/bin/python3
 
-    python3 -m pip install --upgrade pip
-    python3 -m pip install --user ansible
-
+    # clone superbench
     rm -rf ./superbenchmark
     git init superbenchmark
     cd superbenchmark
     git remote add origin https://github.com/microsoft/superbenchmark.git
     git fetch origin
     git checkout -b main origin/main
-    ls -a
+    
+    # create a new virtual environment
+    rm -rf ./venv
+    python3 -m venv --system-site-packages ./venv
+    . ./venv/bin/activate
+
+    python3 -m pip install --upgrade pip setuptools
+
+    # install superbench
     python3 -m pip install .
     make postinstall
-    sb deploy -f ../host.ini --private-key ../private_key.txt
+
+    # make postinstall
+    sb deploy -f ../remote.ini --private-key ../private_key.txt
+    sb run -c ../ib.yaml -f ../remote.ini
 
     # echo "##vso[task.setvariable variable=private_key_file]$private_key_file"    
 }
+hello_py(){
+which python3
+python3 <<-EOF
+print("Hello python")
+EOF
+}
 
+# hello_py
 # To allow agents to gain access to the VMSS
 deploy_superbench
+hello_py
